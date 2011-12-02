65b777eb5485d9d65b7bdcda6690650bce0594cd
Gilberto Cuba <gcubar2005@gmail.com>
2011-12-02 06:31:57 -0500

    android user class renderer v0.4, fixing idiom bug

---
+++ xss/production/android/AnimTest/src/com/xkp/android/AnimTest/ActAnimTest.java
@@ -0,0 +1,28 @@
+package com.xkp.android.AnimTest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import android.view.View;
+			import android.app.Activity;
+			import android.os.Bundle;
+						import android.widget.Button;
+						import android.text.method.KeyListener;
+						import android.view.View.OnClickListener;
+						import android.view.View.OnLongClickListener;
+						import android.view.MotionEvent;
+						import android.view.View.OnTouchListener;
+						import android.view.View.OnKeyListener;
+						import android.view.View.OnFocusChangeListener;
+		public class ActAnimTest 
+						extends Activity
+{
+				private Button btn1;
+			@Override
+			public void onCreate(Bundle savedInstanceState) {
+				super.onCreate(savedInstanceState);
+				setContentView(R.layout.main);
+				bindViews();
+			}
+			private void bindViews() {
+				btn1 = (Button) findViewById(R.id.btn1);
+	}
+}
