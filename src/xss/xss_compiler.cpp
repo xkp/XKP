d6054334fb5871c68584dae0b2522b67a501c1c0
Emilio Santos <xkp.santos@gmail.com>
2011-12-03 17:56:02 -0500

    bf

--- src/xss/xss_compiler.cpp
+++ src/xss/xss_compiler.cpp
@@ -1471,6 +1471,9 @@ void xss_compiler::preprocess_type(XSSType clazz, XSSObject def_class, const str
       {
         virtual void handle(XSSObject obj, XSSModule mod)
           {
+            if (mod->one_of_us(obj))
+              dynamic_set(obj, "idiom", mod);
+
             if (root == obj)
               return; //do not register the class
 
@@ -1478,9 +1481,6 @@ void xss_compiler::preprocess_type(XSSType clazz, XSSObject def_class, const str
             if (!obj_id.empty())
               ctx->register_symbol(RESOLVE_INSTANCE, obj_id, obj);
                     
-            if (mod->one_of_us(obj))
-              dynamic_set(obj, "idiom", mod);
-            
             if (module == mod)
               target->register_instance(obj);
             else
