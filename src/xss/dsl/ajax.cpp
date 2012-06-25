a6118dffa468746265f82c53e5dac075834c1cc4
Emilio Santos <xkp.santos@gmail.com>
2012-06-25 07:45:20 -0400

    ajax files

---
+++ src/xss/dsl/ajax.cpp
@@ -0,0 +1,26 @@
+
+#include <xss/dsl/ajax.h>
+#include <xss/lang/waxjs.h>
+
+using namespace xkp;
+
+//dsl_native
+str dsl_ajax::render(dsl& info, XSSContext ctx)
+  {
+    XSSContext my_ctx(new xss_context(ctx));
+    Language lang(new waxjs_lang);
+
+    my_ctx->set_language(lang);
+    lang->init_application_context(my_ctx);
+    
+    std::vector<str> dl;
+    my_ctx->collect_xss_dsls(dl);
+
+    xs_compiler xc(dl);
+    code ajax_code;
+    xc.compile_code(info.text, ajax_code);
+
+    CodeRenderer rend = lang->compile_code(ajax_code, param_list_decl(), my_ctx);
+    str result = rend->render();
+    return result;
+  }
