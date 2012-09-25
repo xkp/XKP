dd01e43930d97b377e95016b9c50a87d5be791c7
Gilberto Cuba <gcubar2005@gmail.com>
2012-09-25 17:53:31 -0400

    adding wpf control to toolbox of projects WPF in Excess IDE

---
+++ third_party/ve/Excess.Controls/Excess.Switch/ToolboxControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Excess.Switch
+{
+    /// <summary>
+    /// Interaction logic for ToolboxControl.xaml
+    /// </summary>
+    [ProvideToolboxControl("General", true)]
+    public partial class Switch : UserControl
+    {
+        public Switch()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(string.Format(System.Globalization.CultureInfo.CurrentUICulture, "We are inside {0}.button1_Click()", this.ToString()));
+        }
+    }
+}
